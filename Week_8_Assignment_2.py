5242c73891fa599898eff32df3ef55a29f134ae0
zzeraku <zzerakujenkins@gmail.com>
2022-11-26 07:30:57 -0500

    Add project files.

---
+++ Week_8_Assignment_2.py
@@ -0,0 +1,18 @@
+# Brandon Hayashida
+# CIS261
+# Week 8 Assignment 2
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+def main():
+    for i in range(16):
+        print(fib(i), end=", ")
+    print("...")
+
+if __name__ == "__main__":
+    main()
